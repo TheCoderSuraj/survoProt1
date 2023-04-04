14d16bb2649ef92cb05f00f9692f86f0b84ff434
thecodersuraj <surajthecoder@gmail.com>
2023-04-04 21:02:50 +0545

    setup minor error ressolve

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'core/general/screens/home_screen.dart';
 
+void main() {
+  runApp(const SurvoProt1App());
+}
+
 class SurvoProt1App extends StatelessWidget {
   const SurvoProt1App({super.key});
 
